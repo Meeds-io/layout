6bba9a6984f3d66caccc23d0c962b940b77d86e5
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-03-18 15:28:15 +0100

    feat: Export Site templates - MEED-8421 - Meeds-io/MIPs#180 (#384)
    This PR will allow admins to export sites templates.

---
+++ layout-webapp/src/main/webapp/vue-app/site-template/components/action/BulkDelete.vue
@@ -0,0 +1,76 @@
+<!--
+
+  This file is part of the Meeds project (https://meeds.io/).
+
+  Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <div class="d-inline">
+    <v-tooltip :disabled="!$root.systemSelectedSiteTemplates" bottom>
+      <template #activator="{ on, attrs }">
+        <div
+          v-on="on"
+          v-bind="attrs">
+          <v-btn
+            :disabled="$root.isBulkProcessing || $root.systemSelectedSiteTemplates"
+            color="error"
+            elevation="0"
+            outlined
+            @click="openConfirmDialog">
+            <v-icon size="16" class="me-2">fa-trash</v-icon>
+            {{ $t('siteTemplates.label.delete') }}
+          </v-btn>
+        </div>
+      </template>
+      <span>{{ $t('siteTemplates.label.system.noDelete') }}</span>
+    </v-tooltip>
+    <confirm-dialog
+      ref="dialog"
+      :title="$t('siteTemplates.label.bulk.deleteConfirmTitle')"
+      :message="$t('siteTemplates.label.bulk.deleteConfirmMessage')"
+      :ok-label="$t('siteTemplates.label.confirm')"
+      :cancel-label="$t('siteTemplates.label.cancel')"
+      @ok="deleteSiteTemplates"
+      @closed="close" />
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    openConfirmDialog() {
+      window.setTimeout(() => this.$refs.dialog.open(), 200);
+    },
+    deleteSiteTemplates() {
+      this.$root.applyOperationInBulk(
+        siteTemplate => {
+          if (!siteTemplate.system) {
+            return this.$siteTemplateService.deleteSiteTemplate(siteTemplate.id);
+          }
+          return Promise.resolve();
+        },
+        null,
+        () => {
+          this.$root.$emit('alert-message', this.$root.$t('siteTemplates.label.bulk.delete.success'), 'success');
+          this.$root.$emit('site-templates-list-refresh');
+        });
+    },
+    close() {
+      window.setTimeout(() => this.dialog = false, 200);
+    },
+  },
+};
+</script>
