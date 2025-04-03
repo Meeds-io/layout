98cd793fed4d92d8d94dbd098f27012fcd35f6a2
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-04-03 17:17:07 +0200

    fix: Fix importing templates name and descriptions labels - MEED-8710 - Meeds-io/MIPs#180 (#393)
    This PR will fix importing templates name and descriptions labels

--- layout-service/src/main/java/io/meeds/layout/plugin/SiteTemplateDatabindPlugin.java
+++ layout-service/src/main/java/io/meeds/layout/plugin/SiteTemplateDatabindPlugin.java
@@ -39,7 +39,6 @@ import io.meeds.layout.plugin.attachment.SiteTemplateAttachmentPlugin;
 import io.meeds.layout.plugin.translation.PageTemplateTranslationPlugin;
 import io.meeds.layout.plugin.translation.SiteTemplateTranslationPlugin;
 import io.meeds.layout.service.SiteTemplateService;
-import io.meeds.layout.service.injection.LayoutTranslationImportService;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -70,35 +69,32 @@ import lombok.SneakyThrows;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class SiteTemplateDatabindPlugin implements DatabindPlugin {
 
-  private static final Random            RANDOM      = new Random();
+  private static final Random RANDOM      = new Random();
 
-  public static final String             OBJECT_TYPE = "SiteTemplate";
-
-  @Autowired
-  private SiteTemplateService            siteTemplateService;
+  public static final String  OBJECT_TYPE = "SiteTemplate";
 
   @Autowired
-  private DatabindService                databindService;
+  private SiteTemplateService siteTemplateService;
 
   @Autowired
-  private FileService                    fileService;
+  private DatabindService     databindService;
 
   @Autowired
-  private LayoutTranslationImportService layoutTranslationService;
+  private FileService         fileService;
 
   @Autowired
-  private TranslationService             translationService;
+  private TranslationService  translationService;
 
   @Autowired
-  private AttachmentService              attachmentService;
+  private AttachmentService   attachmentService;
 
   @Autowired
-  private UserACL                        userAcl;
+  private UserACL             userAcl;
 
   @Autowired
-  private IdentityManager                identityManager;
+  private IdentityManager     identityManager;
 
-  private long                           superUserIdentityId;
+  private long                superUserIdentityId;
 
   @PostConstruct
   public void init() {
@@ -159,15 +155,12 @@ public class SiteTemplateDatabindPlugin implements DatabindPlugin {
   }
 
   public CompletableFuture<DatabindReport> deserialize(File zipFile, Map<String, String> params, String username) {
-    return CompletableFuture.supplyAsync(() -> importSiteTemplates(zipFile, username))
-                            .thenCompose(processedTemplates -> layoutTranslationService.postImport(SiteTemplateTranslationPlugin.OBJECT_TYPE)
-                                                                                       .thenApply(v -> {
-                                                                                         DatabindReport report =
-                                                                                                               new DatabindReport();
-                                                                                         report.setSuccess(!processedTemplates.isEmpty());
-                                                                                         report.setProcessedItems(processedTemplates);
-                                                                                         return report;
-                                                                                       }));
+    return CompletableFuture.supplyAsync(() -> importSiteTemplates(zipFile, username)).thenApply(processedTemplates -> {
+      DatabindReport report = new DatabindReport();
+      report.setSuccess(!processedTemplates.isEmpty());
+      report.setProcessedItems(processedTemplates);
+      return report;
+    });
 
   }
 
@@ -243,18 +236,28 @@ public class SiteTemplateDatabindPlugin implements DatabindPlugin {
     }
   }
 
+  @SneakyThrows
   private void saveNames(SiteTemplateDatabind siteTemplateDatabind, SiteTemplate siteTemplate) {
-    layoutTranslationService.saveTranslationLabels(SiteTemplateTranslationPlugin.OBJECT_TYPE,
-                                                   siteTemplate.getId(),
-                                                   SiteTemplateTranslationPlugin.TITLE_FIELD_NAME,
-                                                   siteTemplateDatabind.getNames());
+    translationService.saveTranslationLabels(SiteTemplateTranslationPlugin.OBJECT_TYPE,
+                                             siteTemplate.getId(),
+                                             SiteTemplateTranslationPlugin.TITLE_FIELD_NAME,
+                                             siteTemplateDatabind.getNames()
+                                                                 .entrySet()
+                                                                 .stream()
+                                                                 .collect(Collectors.toMap(entry -> Locale.forLanguageTag(entry.getKey()),
+                                                                                           Map.Entry::getValue)));
   }
 
+  @SneakyThrows
   private void saveDescriptions(SiteTemplateDatabind siteTemplateDatabind, SiteTemplate siteTemplate) {
-    layoutTranslationService.saveTranslationLabels(SiteTemplateTranslationPlugin.OBJECT_TYPE,
-                                                   siteTemplate.getId(),
-                                                   SiteTemplateTranslationPlugin.DESCRIPTION_FIELD_NAME,
-                                                   siteTemplateDatabind.getDescriptions());
+    translationService.saveTranslationLabels(SiteTemplateTranslationPlugin.OBJECT_TYPE,
+                                             siteTemplate.getId(),
+                                             SiteTemplateTranslationPlugin.DESCRIPTION_FIELD_NAME,
+                                             siteTemplateDatabind.getDescriptions()
+                                                                 .entrySet()
+                                                                 .stream()
+                                                                 .collect(Collectors.toMap(entry -> Locale.forLanguageTag(entry.getKey()),
+                                                                                           Map.Entry::getValue)));
   }
 
   @SneakyThrows
