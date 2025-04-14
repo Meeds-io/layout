dd06df9e8bf2c65762a3504358cfd10a690a9d34
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-04-14 11:47:41 +0200

    feat: Export space template navigation - MEED-8705 - Meeds-io/MIPs#180 (#396)

--- layout-service/src/main/java/io/meeds/layout/model/NavigationUpdateModel.java
+++ layout-service/src/main/java/io/meeds/layout/model/NavigationUpdateModel.java
@@ -20,9 +20,11 @@ package io.meeds.layout.model;
 
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class NavigationUpdateModel {
 
   private String              nodeLabel;
