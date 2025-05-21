b4b62f753c959983b2a27b9c5af3792122a15031
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-05-21 14:57:04 +0200

    feat: Increment page template import version - MEED-2129 - Meeds-io/MIPs#194 (#431)
    This change will increment page template import version

--- layout-service/src/main/java/io/meeds/layout/service/injection/PageTemplateImportService.java
+++ layout-service/src/main/java/io/meeds/layout/service/injection/PageTemplateImportService.java
@@ -104,7 +104,7 @@ public class PageTemplateImportService {
   @Value("${meeds.pages.import.override:false}")
   private boolean                        forceReimportTemplates;
 
-  @Value("${meeds.pages.import.version:2}")
+  @Value("${meeds.pages.import.version:3}")
   private long                           pageTemplateImportVersion;
 
   @PostConstruct
