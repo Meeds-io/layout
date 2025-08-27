491e27b906b073621cfb08e57638fe2690dd5980
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-08-27 12:21:27 +0100

    feat: Improve TranslationService API with Object Id of Type String - MEED-9632 - Meeds-io/meeds#3608 This change will enhance TranslationService by accepting the objectId of type String in addition to type Long.

--- layout-service/src/test/java/io/meeds/layout/plugin/translation/PageTemplateTranslationPluginTest.java
+++ layout-service/src/test/java/io/meeds/layout/plugin/translation/PageTemplateTranslationPluginTest.java
@@ -61,28 +61,28 @@ public class PageTemplateTranslationPluginTest {
   @Test
   @SneakyThrows
   void hasEditPermission() {
-    assertFalse(translationPlugin.hasEditPermission(0l, null));
-    assertFalse(translationPlugin.hasEditPermission(0l, username));
+    assertFalse(translationPlugin.hasEditPermission("0", null));
+    assertFalse(translationPlugin.hasEditPermission("0", username));
     when(layoutAclService.isAdministrator(username)).thenReturn(true);
-    assertTrue(translationPlugin.hasEditPermission(0l, username));
+    assertTrue(translationPlugin.hasEditPermission("0", username));
   }
 
   @Test
   @SneakyThrows
   void hasAccessPermission() {
-    assertTrue(translationPlugin.hasAccessPermission(0l, null));
+    assertTrue(translationPlugin.hasAccessPermission("0", null));
   }
 
   @Test
   @SneakyThrows
   void getAudienceId() {
-    assertEquals(0l, translationPlugin.getAudienceId(0l));
+    assertEquals(0l, translationPlugin.getAudienceId("0"));
   }
 
   @Test
   @SneakyThrows
   void getSpaceId() {
-    assertEquals(0l, translationPlugin.getSpaceId(0l));
+    assertEquals(0l, translationPlugin.getSpaceId("0"));
   }
 
 }
