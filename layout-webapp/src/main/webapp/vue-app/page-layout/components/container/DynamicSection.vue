5693e03d504281b42a378f2e601a5c2831d25f3a
sofyenne <shajchedli@exoplatform.com>
2026-06-09 17:13:35 +0100

    fix: Improve mobile section column access up to md breakpoint - EXO-87567

--- layout-webapp/src/main/webapp/vue-app/page-layout/components/container/DynamicSection.vue
+++ layout-webapp/src/main/webapp/vue-app/page-layout/components/container/DynamicSection.vue
@@ -55,8 +55,8 @@ export default {
   }),
   computed: {
     isMobileColumns() {
-      return this.$vuetify.breakpoint.smAndDown
-        && this.container?.cssClass?.includes?.('layout-mobile-columns');
+      return ['xs', 'sm', 'md'].includes(this.$vuetify.breakpoint?.name)
+          && this.container?.cssClass?.includes?.('layout-mobile-columns');
     },
     isHiddenOnMobile() {
       return this.$vuetify.breakpoint.smAndDown
