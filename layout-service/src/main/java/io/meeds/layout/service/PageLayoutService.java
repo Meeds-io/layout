824a078f996acfd02e24f17fe121a035a839964b
Mohamed Amine Krout <mkrout@exoplatform.com>
2026-07-16 15:40:19 +0100

    feat: Include content block in the search embedding - EXO-88570
    This commit broadcasts page layout/permissions update events for
    reactive indexing.

--- layout-service/src/main/java/io/meeds/layout/service/PageLayoutService.java
+++ layout-service/src/main/java/io/meeds/layout/service/PageLayoutService.java
@@ -48,6 +48,7 @@ import org.exoplatform.portal.mop.page.PageKey;
 import org.exoplatform.portal.mop.page.PageState;
 import org.exoplatform.portal.mop.service.LayoutService;
 import org.exoplatform.portal.pom.spi.portlet.Portlet;
+import org.exoplatform.services.listener.ListenerService;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 
@@ -63,6 +64,12 @@ import lombok.SneakyThrows;
 
 @Service
 public class PageLayoutService {
+
+  /** Broadcast after a page's layout has been persisted (content added/removed/reordered). */
+  public static final String     PAGE_UPDATED_EVENT              = "layout.page.updated";
+
+  /** Broadcast after a page's access/edit permissions have been persisted. */
+  public static final String     PAGE_PERMISSIONS_UPDATED_EVENT  = "layout.page.permissions.updated";
 
   private static final String    ADDON_CONTAINER_FACTORY_ID      = "addonContainer";
 
@@ -96,6 +103,9 @@ public class PageLayoutService {
 
   @Autowired
   private UserPortalConfigService portalConfigService;
+
+  @Autowired
+  private ListenerService        listenerService;
 
   public List<PageContext> getPages(String siteTypeName,
                                     String siteName,
@@ -350,6 +360,7 @@ public class PageLayoutService {
     validateCSSInputs(page);
     existingPage.setChildren(page.getChildren());
     layoutService.save(existingPage);
+    listenerService.broadcast(PAGE_UPDATED_EVENT, username, pageKey.format());
     return layoutService.getPageContext(existingPage.getPageKey());
   }
 
@@ -423,6 +434,7 @@ public class PageLayoutService {
     pageState.setAccessPermissions(permissionUpdateModel.getAccessPermissions());
     pageState.setEditPermission(permissionUpdateModel.getEditPermission());
     layoutService.save(pageContext);
+    listenerService.broadcast(PAGE_PERMISSIONS_UPDATED_EVENT, username, pageKey.format());
   }
 
   public void impersonateModel(ModelObject object) {
