e96db95df437a8d19909954805cd4506a3e25fba
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2026-07-17 15:48:50 +0100

    Revert "fix: Fix restore default pages option - EXO-88526 - eXIP7.3.0.9 (#543)"
    This reverts commit 1d93f87d93789ce0cd478e69c025853d2a37444e.

--- layout-webapp/src/main/webapp/vue-app/site-management/components/main/Menu.vue
+++ layout-webapp/src/main/webapp/vue-app/site-management/components/main/Menu.vue
@@ -339,7 +339,7 @@ export default {
         await this.$siteLayoutService.restoreSite({
           siteName: this.site.name,
           siteType: this.site.siteType,
-          importMode: 'OVERWRITE',
+          importMode: 'MERGE',
           siteLayout: false,
           sitePages: true,
           siteNavigation: true,
